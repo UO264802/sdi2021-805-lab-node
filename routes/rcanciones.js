22c0743c85a04fd8fa6205a6fd42c04d36f40898
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-03-23 12:49:55 +0100

    SDI-2021-805-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,32 +1,50 @@
-module.exports = function(app) {
-    app.get("/canciones", function(req, res) {
-        let respuesta = "";
-        if (req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-        if (typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: '+ req.query.autor;
+module.exports = function (app, swig) {
+    app.get("/canciones", function (req, res) {
+        let canciones = [{
+            "nombre": "blank space",
+            "precio": "1.2"
+        }, {
+            "nombre": "See you again",
+            "precio": "1.3"
+        }, {
+            "nombre": "Uptown Funk",
+            "precio": "1.1"
+        }];
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+        });
         res.send(respuesta);
     });
 
-    app.get('/canciones/:id', function(req, res) {
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
+
+    app.get('/canciones/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
-    app.get('/canciones/:genero/:id', function(req, res) {
+    app.get('/canciones/:genero/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
     });
 
 
-    app.get('/suma', function(req, res) {
+    app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
 
-    app.post("/cancion", function (req, res){
+    app.post("/cancion", function (req, res) {
         res.send("Cancion agregada:" + req.body.nombre + "<br>"
-        + " genero :" + req.body.genero + "<br>"
-        + " precio: " + req.body.precio);
+            + " genero :" + req.body.genero + "<br>"
+            + " precio: " + req.body.precio);
     });
+
+
 };
