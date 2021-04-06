33da70225bce11378a795c6b7257668048e4ff91
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-04-06 10:10:45 +0200

    SDI-2021-805-8.3-Subida a ficheros y gestión de colecciones.

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ let app = express();
 let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 // Variables
 app.set('port', 8081);
@@ -22,8 +27,7 @@ require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, 
 require("./routes/rcanciones.js")(app, swig, gestorBD);// (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig);
 
-let gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+
 
 //Lanzar el servidor
 app.listen(app.get('port'), function (){
