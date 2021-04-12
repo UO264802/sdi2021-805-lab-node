bf9d206efb198be2041f044909a280132aaf36a0
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-04-12 23:43:35 +0200

    SDI-2021-805-9.6-Manejo de errores y HTTPS.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 let express = require('express');
 let app = express();
 
+let fs = require('fs');
+let https = require('https');
+
 let expressSession = require('express-session');
 app.use(expressSession({
     secret: 'abcdefg',
@@ -65,8 +68,8 @@ routerUsuarioAutor.use(function (req, res, next) {
 //Aplicar routerUsuarioAutor
 app.use("/cancion/modificar", routerUsuarioAutor);
 app.use("/cancion/eliminar", routerUsuarioAutor);
-app.use("/cancion/comprar",routerUsuarioSession);
-app.use("/compras",routerUsuarioSession);
+app.use("/cancion/comprar", routerUsuarioSession);
+app.use("/compras", routerUsuarioSession);
 
 //routerAudios
 let routerAudios = express.Router();
@@ -80,12 +83,12 @@ routerAudios.use(function (req, res, next) {
                 next();
             } else {
                 let criterio = {
-                    usuario : req.session.usuario,
-                    cancionId : mongo.ObjectID(idCancion)
+                    usuario: req.session.usuario,
+                    cancionId: mongo.ObjectID(idCancion)
                 };
 
-                gestorBD.obtenerCompras(criterio ,function(compras){
-                    if (compras != null && compras.length > 0 ){
+                gestorBD.obtenerCompras(criterio, function (compras) {
+                    if (compras != null && compras.length > 0) {
                         next();
                     } else {
                         res.redirect("/tienda");
@@ -114,7 +117,19 @@ app.get('/', function (req, res) {
     res.redirect('/tienda');
 })
 
+app.use(function (err, req, res, next) {
+    console.log("Error producido: " + err);
+    if (!res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
+
+
 //Lanzar el servidor
-app.listen(app.get('port'), function () {
-    console.log('servidor activo');
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function() {
+    console.log("Servidor activo");
 });
