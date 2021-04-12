e1c425a4f9dc6e5a46763b45c06c5a1d7e083d91
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-04-12 16:50:09 +0200

    SDI-2021-805-9.1-Gestión de colecciones, eliminar documento.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //mongodb://admin:sdi@tiendamusica-shard-00-00.gu0l8.mongodb.net:27017,
 // tiendamusica-shard-00-01.gu0l8.mongodb.net:27017,
 // tiendamusica-shard-00-02.gu0l8.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hld8dh-shard-0&authSource=admin&retryWrites=true&w=majority
@@ -22,38 +21,60 @@ let crypto = require('crypto');
 
 app.use(fileUpload());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
 let gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+gestorBD.init(app, mongo);
 
 // routerUsuarioSession
 var routerUsuarioSession = express.Router();
-routerUsuarioSession.use(function(req, res, next) {
+routerUsuarioSession.use(function (req, res, next) {
     console.log("routerUsuarioSession");
-    if ( req.session.usuario ) {
+    if (req.session.usuario) {
         // dejamos correr la petición
         next();
     } else {
-        console.log("va a : "+req.session.destino)
+        console.log("va a : " + req.session.destino)
         res.redirect("/identificarse");
     }
 });
 //Aplicar routerUsuarioSession
-app.use("/canciones/agregar",routerUsuarioSession);
-app.use("/publicaciones",routerUsuarioSession);
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+
+//routerUsuarioAutor
+let routerUsuarioAutor = express.Router();
+routerUsuarioAutor.use(function (req, res, next) {
+    console.log("routerUsuarioAutor");
+    let path = require('path');
+    let id = path.basename(req.originalUrl);
+// Cuidado porque req.params no funciona
+// en el router si los params van en la URL.
+    gestorBD.obtenerCanciones(
+        {_id: mongo.ObjectID(id)}, function (canciones) {
+            console.log(canciones[0]);
+            if (canciones[0].autor == req.session.usuario) {
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerUsuarioAutor
+app.use("/cancion/modificar", routerUsuarioAutor);
+app.use("/cancion/eliminar", routerUsuarioAutor);
 
 //routerAudios
 let routerAudios = express.Router();
-routerAudios.use(function(req, res, next) {
+routerAudios.use(function (req, res, next) {
     console.log("routerAudios");
     let path = require('path');
     let idCancion = path.basename(req.originalUrl, '.mp3');
     gestorBD.obtenerCanciones(
-        {"_id": mongo.ObjectID(idCancion) }, function (canciones) {
-            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+        {"_id": mongo.ObjectID(idCancion)}, function (canciones) {
+            if (req.session.usuario && canciones[0].autor == req.session.usuario) {
                 next();
             } else {
                 res.redirect("/tienda");
@@ -61,15 +82,15 @@ routerAudios.use(function(req, res, next) {
         })
 });
 //Aplicar routerAudios
-app.use("/audios/",routerAudios);
+app.use("/audios/", routerAudios);
 
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.gu0l8.mongodb.net:27017,tiendamusica-shard-00-01.gu0l8.mongodb.net:27017,tiendamusica-shard-00-02.gu0l8.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hld8dh-shard-0&authSource=admin&retryWrites=true&w=majority');
-app.set('clave','abcdefg');
-app.set('crypto',crypto);
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD);// (app, param1, param2, etc.)
@@ -77,8 +98,7 @@ require("./routes/rautores.js")(app, swig);
 require("./routes/rcomentarios")(app, swig, gestorBD);
 
 
-
 //Lanzar el servidor
-app.listen(app.get('port'), function (){
+app.listen(app.get('port'), function () {
     console.log('servidor activo');
 });
