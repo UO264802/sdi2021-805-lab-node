5902532c4f3e474d2c9cbc9180caa947f63fa2df
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-04-20 11:21:40 +0200

    SDI-2021-805-10.6-SW Servicio Memoria.

--- helloworld-server.js
+++ helloworld-server.js
@@ -1,4 +1,24 @@
 let http = require('http');
+let express = require('express');
+let app = express();
+let os = require('os');
+let puerto = 3000;
+app.get('/memoria', function(req, res){
+    setTimeout(function() { // Espera de 10 segundos
+
+        console.log(os.freemem());
+        var memoriaLibre = os.freemem() / 1000000; //pasar a MB
+        res.status(200);
+        res.json({
+            memoria : memoriaLibre
+        });
+
+    }, 10000);
+});
+app.listen(puerto, function() {
+    console.log("Servidor listo "+puerto);
+});
+
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
