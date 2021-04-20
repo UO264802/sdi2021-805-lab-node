e2537e550de93e03f0c132945df9daa17d84fc74
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-04-20 09:43:46 +0200

    SDI-2021-805-10.1-SW API REST CRUD Canciones.

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ app.set('crypto', crypto);
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD);// (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig);
-require("./routes/rcomentarios")(app, swig, gestorBD);
+require("./routes/rcomentarios.js")(app, swig, gestorBD);
+require("./routes/rapicanciones.js")(app, gestorBD);
 
 app.get('/', function (req, res) {
     res.redirect('/tienda');
